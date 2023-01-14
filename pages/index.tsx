cfccfb12cf0ae5c7eaeb92e2b1a8f731e8532d88
Jingyue <jingyue804@gmail.com>
2023-01-14 15:24:02 -0500

    Update home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,22 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <>
-      <div className="landing-container">
-        <img src="/img/hero-background.png" alt="" />
-        <div className="brand">StudySpot</div>
+      <header>
+        <h1 className="brand">StudySpot</h1>
+        <div className="navlinks">
+          <a href="">Search</a>
+          <a href="">Leaderboard</a>
+        </div>
+      </header>
+      <img src="/img/hero-background.png" alt="" className="background" />
+
+      <div className="content">
+        <div className="card">
+          <h2 className="title">Time Spent Studying</h2>
+          <h1 className="timer">Time</h1>
+          <h2 className="studyscore">StudyScore: </h2>
+          <h2 className="location">H.G. Thode Library</h2>
+        </div>
       </div>
     </>
   );
