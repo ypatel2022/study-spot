63a7d78d8ca8be8fed77176cf499f23ae8ce3cd1
ColinChambachan <colin.chambachan@gmail.com>
2023-01-14 15:01:21 -0500

    Creates "Nearby places" API Call

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
 
-type Data = {
-  name: string
-}
+export default async function handler(req: NextApiRequest, res: any) {
+  var config = {
+    method: "get",
+    url: `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=43.2631%2C-79.9212&radius=10&type=library&keyword=library&key=${process.env.MAPS_API_KEY}`,
+    headers: {},
+  };
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+  let data = {};
+
+  await axios(config)
+    //@ts-ignore
+    .then(function (response) {
+      data = response.data;
+    })
+    //@ts-ignore
+    .catch(function (error) {
+      console.log(error);
+    });
+
+  res.status(200).json(data);
 }
