e190820ee15d01658ca06ca7f3d6558171596c5d
Jinwoo-H <jinwoo0825@gmail.com>
2023-01-14 17:36:32 -0500

    add timer feature and studyscore

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,36 @@
 import Login from '../components/Login'
+import Head from 'next/head'
+import Image from 'next/image'
+import React, { useEffect, useState } from 'react'
+import { getPositionOfLineAndCharacter } from 'typescript'
+import { secondsToTime, studyScore } from '../utils/time'
 
 export default function Home() {
+  const [latitude, setLatitude] = useState(0)
+  const [longitude, setLongitude] = useState(0)
+  const [time, setTime] = useState(0)
+  const [enterTime, setEnterTime] = useState(1673731254882)
+  const [currentTime, setCurrentTime] = useState(0)
+  const [hours, setHours] = useState(0)
+  const [minutes, setMinutes] = useState(0)
+  const [seconds, setSeconds] = useState(0)
+  const [elapsed, setElapsed] = useState(0)
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(function (position) {
+      console.log('Latitude is :', position.coords.latitude)
+      console.log('Longitude is :', position.coords.longitude)
+      setLatitude(position.coords.latitude)
+      setLongitude(position.coords.longitude)
+      position.coords.accuracy
+    })
+
+    setInterval(() => {
+      console.log('This will run every second!')
+      setCurrentTime(Date.now())
+    }, 1000)
+  }, [])
+
   return (
     <>
       <header className="w-full">
@@ -16,8 +46,13 @@ export default function Home() {
       <div className="content">
         <div className="card">
           <h2 className="title">Time Spent Studying</h2>
-          <h1 className="timer">Time</h1>
-          <h2 className="studyscore">StudyScore: </h2>
+          <h1 className="timer">
+            {secondsToTime(Math.round((currentTime - enterTime) / 1000))}
+          </h1>
+          <h2 className="studyscore">
+            StudyScore:{' '}
+            {studyScore(Math.round((currentTime - enterTime) / 1000))}
+          </h2>
           <h2 className="location">H.G. Thode Library</h2>
         </div>
       </div>
