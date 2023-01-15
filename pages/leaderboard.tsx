c497f25775c49de7363d6ad463b37cbaab63d64b
Jingyue <jingyue804@gmail.com>
2023-01-15 10:16:01 -0500

    Update leaderboard

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { auth, getAllLogs, getName } from '../utils/firebase'
 import { useAuthState } from 'react-firebase-hooks/auth'
-import Navbar from '../components/Navbar'
 
 export default function leaderboard() {
   const [user, loading, error] = useAuthState(auth)
@@ -59,23 +58,28 @@ export default function leaderboard() {
 
   return (
     <div>
+      <header>
+        <h1 className='brand'>
+          <Link href='/'>StudySpot</Link>
+        </h1>
+        <div className='navlinks'>
+          <a href=''>Leaderboard</a>
+        </div>
+      </header>
       <img
         src='/img/hero-background.png'
         alt=''
         className='background'
         draggable='false'
       />
-
-      <Navbar loggedOut={true} />
-
       <div className='content'>
         <div className='leaderboardcard'>
           <h1 className='leaderboardtitle'>Leaderboard</h1>
           <div className='legend'></div>
 
           <div className='relative overflow-x-auto'>
-            <table className='w-full text-sm text-left text-gray-500'>
-              <thead className='text-xs text-gray-700 uppercase bg-gray-50'>
+            <table className='w-full text-sm text-left text-gray-500 dark:text-gray-400'>
+              <thead className='text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400'>
                 <tr>
                   <th scope='col' className='px-6 py-3'>
                     User
@@ -91,10 +95,10 @@ export default function leaderboard() {
                 {users &&
                   users.map((user) => {
                     return (
-                      <tr className='bg-white border-b'>
+                      <tr className='bg-white border-b dark:bg-gray-800 dark:border-gray-700'>
                         <th
                           scope='row'
-                          className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap '
+                          className='px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white'
                         >
                           {user.uid}
                         </th>
