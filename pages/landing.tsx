96ffda86d310b852ce8b582c63f6bc5c38c33c36
Jingyue <jingyue804@gmail.com>
2023-01-15 00:53:02 -0500

    Update landing page

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -3,20 +3,24 @@ import Link from "next/link";
 import Login from "../components/Login";
 
 export default function landing() {
-      
+    
   return (
     <div>
-      <img src="/img/hero-background.png" alt="" className="background" />
+      <img
+        src="/img/hero-background.png"
+        alt=""
+        className="landingbackground"
+      />
 
-        <div className="landingnavbar">
-          <h1 className="landingbrand">
-            <Link href="">StudySpot</Link>
-          </h1>
-          <div className="Lnavlinks">
-            <Link href="">Features</Link>
-            <Link href="">Login</Link>
-          </div>
+      <div className="landingnavbar">
+        <h1 className="landingbrand">
+          <Link href="">StudySpot</Link>
+        </h1>
+        <div className="Lnavlinks">
+          <Link href="">Features</Link>
+          <Link href="">Login</Link>
         </div>
+      </div>
 
       <div className="landingintro">
         <div className="introinfo">
@@ -28,17 +32,52 @@ export default function landing() {
           <button type="submit" className="signup-button">
             Sign Up For Free
           </button>
-
         </div>
         <div className="landingrightintro">
           <img src="/img/map.png" alt="" className="mapgraphic" />
         </div>
       </div>
 
+      <img src="/img/wave.svg" alt="" className="wave" />
       <div className="infopage">
+        <h1 className="infotitle">How it Works</h1>
 
-        
+        <div className="howitworks">
+          <div className="block">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              1. Locate Study locations
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Find a library and begin studying.
+            </p>
+          </div>
+          <div className="block">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              2. Track time spent studying{" "}
+            </h5>
+            <p className="block">
+              StudySpot automatically tracks your productivity and gives you a
+              score.{" "}
+            </p>
+          </div>
+          <div className="block">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              3. Move up the leader board
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Compete against others to boost your productivity.{" "}
+            </p>
+          </div>
 
+          <div className="block">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+              4. Show off your own NFTs
+            </h5>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              Mint StudySpot NFTs with unique metadata
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
