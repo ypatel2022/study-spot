105c6a12e37ad6038f1fd2427ab99bffbfb0632b
Jingyue <jingyue804@gmail.com>
2023-01-14 23:46:44 -0500

    Update landing page

--- pages/landing.tsx
+++ pages/landing.tsx
@@ -3,20 +3,43 @@ import Link from "next/link";
 import Login from "../components/Login";
 
 export default function landing() {
+      
   return (
     <div>
+      <img src="/img/hero-background.png" alt="" className="background" />
 
-      <header>
-        <h1 className="brand">
-          <Link href="">StudySpot</Link>
-        </h1>
-        <div className="Lnavlinks">
-          <Link href="">About</Link>
-          <Link href="">Login</Link>
+        <div className="landingnavbar">
+          <h1 className="landingbrand">
+            <Link href="">StudySpot</Link>
+          </h1>
+          <div className="Lnavlinks">
+            <Link href="">Features</Link>
+            <Link href="">Login</Link>
+          </div>
         </div>
-      </header>
 
-      <img src="/img/hero-background.png" alt="" className="background"/>
+      <div className="landingintro">
+        <div className="introinfo">
+          <h2 className="landingtitle">Level Up Your Productivity</h2>
+          <p className="landingtext">Discover study spots</p>
+          <p className="landingtext">Get rewarded for studying</p>
+          <p className="landingtext">Web 3 smart contract integration</p>
+
+          <button type="submit" className="signup-button">
+            Sign Up For Free
+          </button>
+
+        </div>
+        <div className="landingrightintro">
+          <img src="/img/map.png" alt="" className="mapgraphic" />
+        </div>
+      </div>
+
+      <div className="infopage">
+
+        
+
+      </div>
     </div>
   );
 }
